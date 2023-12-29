ebd63335dc65019f88166f2b6b2513cc8113425e
Hakyur <drogotovsky@gmail.com>
2023-12-30 00:20:35 +0300

    add class Point2f in math, graphicConveyor

---
+++ src/main/java/ru/vsu/cs/team4/task4/math/Point2f.java
@@ -0,0 +1,34 @@
+package ru.vsu.cs.baklanova.Math;
+
+
+import ru.vsu.cs.baklanova.Math.vector.Vector3f;
+
+public class Point2f {
+    private float x;
+    private float y;
+
+    public Point2f(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public static Point2f vertexToPoint(final Vector3f vertex, final int width, final int height) {
+        return new Point2f(vertex.getX() * width + width / 2.0F, -vertex.getY() * height + height / 2.0F);
+    }
+}
