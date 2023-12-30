87d0cd2160d15bdfa55398edd9c25d24b16ca56a
gitash2 <shapor.art@yandex.ru>
2023-12-30 15:04:05 +0300

    Added GUI, Scene, LoadedModel,Camera classes and RederEngine folder for program to work.

---
+++ src/main/java/ru/vsu/cs/team4/task4/Scene.java
@@ -0,0 +1,19 @@
+package ru.vsu.cs.team4.task4;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Scene {
+
+    private List<LoadedModel> models;
+
+
+    public Scene() {
+        this.models = new ArrayList<>();
+    }
+
+    public List<LoadedModel> getModels() {
+        return models;
+    }
+}
---
+++ src/main/java/ru/vsu/cs/team4/task4/Simple3DViewer.java
@@ -0,0 +1,37 @@
+package ru.vsu.cs.team4.task4;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+
+public class Simple3DViewer extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        AnchorPane viewport = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("fxml/gui.fxml")));
+
+        Scene scene = new Scene(viewport);
+        stage.setMinWidth(1600);
+        stage.setMinHeight(900);
+        viewport.prefWidthProperty().bind(scene.widthProperty());
+        viewport.prefHeightProperty().bind(scene.heightProperty());
+
+
+        InputStream iconStream = getClass().getResourceAsStream("/images/logo.jpg");
+        Image image = new Image(iconStream);
+        stage.getIcons().add(image);
+
+        stage.setTitle("Simple3DViewer");
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch();
+    }
+}
